46ed7299e34b933b7261b9066d3169b597077d7d
Jose Angel Betancourt <91120893+josefdzbeta@users.noreply.github.com>
2022-01-21 18:29:41 +0100

    Update DBcontroller.php

--- DBcontroller.php
+++ DBcontroller.php
@@ -1,5 +1,9 @@
 <?php
+
+    //Procedimientos php
     class DBController {
+        
+        //Datos del servidor 
         private $host = "localhost";
         private $user = "2DAWJoseAngel";
         private $password = "hola1234";
@@ -7,22 +11,23 @@
         
         public $conn;
         
+         
         function __construct() {
             $this->conn = $this->connectDB();
             if(!empty($this->conn)) {
                 $this->selectDB();
             }
         }
-        
+        //conectamos con el servidor
         function connectDB() {
             $conn = mysqli_connect($this->host,$this->user,$this->password,$this->database);
             return $conn;
         }
-        
+        //seleccionamos la base de datos
         function selectDB() {
             mysqli_select_db($this->conn, $this->database);
         }
-        
+        //Ejecutamos la consulta
         function runQuery($query) {
             $result = mysqli_query($this->conn, $query);
             while($row=mysqli_fetch_assoc($result)) {
@@ -31,7 +36,7 @@
             if(!empty($resultset))
                 return $resultset;
         }
-        
+        //Comprobamos la consulta
         function numRows($query) {
             $result  = mysqli_query($this->conn, $query);
             $rowcount = mysqli_num_rows($result);
